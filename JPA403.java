1d8ce0ee420e5472bd2fecc4e5ff0e1244a2e660
Skynight666 <161405183+Skynight666@users.noreply.github.com>
2024-03-22 00:24:50 +0800

    Add files via upload

---
+++ JPA403.java
@@ -0,0 +1,32 @@
+public class JPA403 {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 3}, {4, 5, 6}};
+        int[][] b = {{7, 8, 9}, {10, 11, 12}};
+        int[][] c = new int[3][3];
+        System.out.println("陣列A的內容為(3x3):");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("  "+ a[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("陣列B的內容為(3x3):");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("  "+ b[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("陣列A+B=C的內容為(3x3):");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+                System.out.print("  "+ c[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
